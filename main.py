4fe62f35e66f856d2236e1f8802a49f0d4c227f4
Evon Troy Alexander <evont.alexander@gmail.com>
2023-06-25 20:04:37 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 """In this project, you’ll build a program that will take the weight of a package and
 determine the cheapest way to ship that package using Sal’s Shippers."""
+
+
+def shipping():
+    weight = 0
